f306b7b8a0a0a3447d4ddcb1661597f76f4b3723
Meghanath91 <meghanath.balaji@gmail.com>
2021-01-29 01:13:03 -0500

    add design to book item

--- src/app/components/BookItem.js
+++ src/app/components/BookItem.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { deleteBook } from '../redux/actions'
 import '../../css/bookitem.css'
-
+import deleteBtn from "../../images/delete.webp"
 export default function BookItem({ book }) {
   const dispatch = useDispatch()
   return (
     <div className="bookitem-container">
-      <div className="book-name">{book.name}</div>
-      <div onClick={() => dispatch(deleteBook(book.id))}>Delete</div>
-      <div>{book.category}</div>
-      <div>{book.description}</div>
-      <div>{book.price}</div>
+      <div className="book-name-container">
+        <div className="book-name"> {book.name}</div>
+        <img className="img-del" src={deleteBtn} alt="deletebtn" onClick={() => dispatch(deleteBook(book.id))} />
+      </div>
+
+      <div className="category">Category : {book.category}</div>
+      <div className="desc">{book.description}</div>
+      <div className="price">Price : {book.price} $</div>
     </div>
   )
 }
--- src/app/redux/states.js
+++ src/app/redux/states.js
@@ -1,16 +1,16 @@
 export let books = [
   {
     id: 1,
-    name: 'sherlock Holmes',
+    name: 'Sherlock Holmes',
     category: 'Adventure',
-    price: 35.5,
-    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+    price: 35,
+    description: 'Sherlock Holmes is a fictional private detective created by British author Sir Arthur Conan Doyle. Referring to himself as a "consulting detective" in the stories, Holmes is known for his proficiency ...'
   },
   {
     id: 2,
     name: 'Godfather',
     category: 'Drama',
-    price: 80.0,
-    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+    price: 80,
+    description: 'Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.'
   }
 ]
